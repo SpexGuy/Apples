3d52a365bae902377ed9e51bc25fa0256cdf18c8
Martin Wickham <spexguy070@gmail.com>
2012-09-07 14:47:37 -0500

    Created the Deck<T extends Card> data structure.
    Basically a list, but you can choose a random element.

---
+++ src/edu/madison/a2a/Deck.java
@@ -0,0 +1,29 @@
+package edu.madison.a2a;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Deck<T extends Card> {
+	private List<T> cards;
+	
+	public Deck(List<T> cards) {
+		this.cards = new LinkedList<T>(cards);
+	}
+	
+	public T drawRandomCard() {
+		if (cards.size() == 0) return null;
+		return cards.remove(randomInt(0, cards.size()));
+	}
+	
+	public void add(T card) {
+		cards.add(card);
+	}
+	
+	public void addAll(List<T> cards) {
+		this.cards.addAll(cards);
+	}
+	
+	public static int randomInt(int min, int max) {
+		return (int)(Math.random()*(max-min)+min);
+	}
+}
